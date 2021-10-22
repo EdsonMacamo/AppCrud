8c6ec1f7ea4e374de038461fa25435ef17794537
Edson Macamo <edsonmacamo85@gmail.com>
2021-10-22 15:56:15 +0200

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { HttpClientModule } from '@angular/common/http';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { ProductComponent } from './product/product.component';
 import { DepartmentComponent } from './department/department.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +29,14 @@ import { DepartmentComponent } from './department/department.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Department } from '../department';
+import { DepartmentService } from '../department.service';
 
 @Component({
   selector: 'app-department',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./department.component.css']
 })
 export class DepartmentComponent implements OnInit {
-
-  constructor() { }
+  depName: string = ' ';
+  departments:Department[] = [{name: 'dep 1', _id:"efff"}];
+  constructor(private departmentService:DepartmentService) { }
 
   ngOnInit(): void {
+    this.departmentService.get()
+    .subscribe((deps) => this.departments = deps)
+  }
+  save(){
+   this.departmentService.add({name:this.depName})
+    .subscribe(
+      (dep) => {
+        console.log(dep)
+        this.clearFields()
+      },
+      (err)=> console.error(err)
+    )
+    
+
+  }
+  clearFields(){
+    this.depName = '';
+  }
+  cancel(){
+
+  }
+  delete(id:Department){
+
+  }
+  edit(id:Department){
+
   }
 
 }
